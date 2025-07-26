5801d0f4e14878466d3664d518a3e873e68b993a
Dmitry Meyer <me@undef.im>
2025-07-26 11:32:41 +0000

    test: drop BaseTestClass
    it's not compatible with newer pytest

--- tests/test_path.py
+++ tests/test_path.py
@@ -8,25 +8,25 @@ from pyenv_inspect.path import (
     get_pyenv_versions_directory,
 )
 
-from tests.testlib import BaseTestClass, posix_test, windows_test
+from tests.testlib import posix_test, windows_test
 
 
-class BaseTestGetPyenvRoot(BaseTestClass):
+class BaseTestGetPyenvRoot:
 
     @pytest.fixture(autouse=True)
-    def setup(monkeypatch):
+    def setup(self, monkeypatch):
         for env_var in ['PYENV_ROOT', 'PYENV_HOME', 'PYENV']:
             monkeypatch.delenv(env_var, raising=False)
 
     @pytest.fixture
-    def fake_home(tmp_path, monkeypatch):
+    def fake_home(self, tmp_path, monkeypatch):
         _fake_home = tmp_path / 'fake_home'
         _fake_home.mkdir()
         monkeypatch.setattr('pathlib.Path.home', lambda: _fake_home)
         return _fake_home
 
     @pytest.fixture
-    def pyenv_root_custom(tmp_path):
+    def pyenv_root_custom(self, tmp_path):
         pyenv_root = tmp_path / 'pyenv_custom_path'
         pyenv_root.mkdir()
         return pyenv_root
@@ -35,17 +35,17 @@ class BaseTestGetPyenvRoot(BaseTestClass):
     def pyenv_root_default():
         raise NotImplementedError
 
-    def test_default_location(pyenv_root_default):
+    def test_default_location(self, pyenv_root_default):
         assert get_pyenv_root() == pyenv_root_default
 
-    def test_error_does_not_exist(pyenv_root_default):
+    def test_error_does_not_exist(self, pyenv_root_default):
         pyenv_root_default.rmdir()
         message = f'pyenv root does not exist: {pyenv_root_default}'
 
         with pytest.raises(PathError, match=re.escape(message)):
             get_pyenv_root()
 
-    def test_error_not_a_directory(pyenv_root_default):
+    def test_error_not_a_directory(self, pyenv_root_default):
         pyenv_root_default.rmdir()
         pyenv_root_default.touch()
         message = f'pyenv root is not a directory: {pyenv_root_default}'
@@ -58,18 +58,18 @@ class BaseTestGetPyenvRoot(BaseTestClass):
 class TestGetPyenvRootPOSIX(BaseTestGetPyenvRoot):
 
     @pytest.fixture
-    def pyenv_root_default(fake_home):
+    def pyenv_root_default(self, fake_home):
         pyenv_root = fake_home / '.pyenv'
         pyenv_root.mkdir()
         return pyenv_root
 
-    def test_custom_location(monkeypatch, pyenv_root_custom):
+    def test_custom_location(self, monkeypatch, pyenv_root_custom):
         monkeypatch.setenv('PYENV_ROOT', str(pyenv_root_custom))
 
         assert get_pyenv_root() == pyenv_root_custom
 
     def test_custom_location_from_win_envs_ignored(
-        monkeypatch, pyenv_root_default, pyenv_root_custom,
+        self, monkeypatch, pyenv_root_default, pyenv_root_custom,
     ):
         monkeypatch.setenv('PYENV_HOME', str(pyenv_root_custom))
         monkeypatch.setenv('PYENV', str(pyenv_root_custom))
@@ -81,48 +81,48 @@ class TestGetPyenvRootPOSIX(BaseTestGetPyenvRoot):
 class TestGetPyenvRootWindows(BaseTestGetPyenvRoot):
 
     @pytest.fixture
-    def pyenv_root_default(fake_home):
+    def pyenv_root_default(self, fake_home):
         pyenv_root = fake_home / '.pyenv' / 'pyenv-win'
         pyenv_root.mkdir(parents=True)
         return pyenv_root
 
     @pytest.mark.parametrize('env_var', ['PYENV_ROOT', 'PYENV_HOME', 'PYENV'])
-    def test_custom_location(monkeypatch, pyenv_root_custom, env_var):
+    def test_custom_location(self, monkeypatch, pyenv_root_custom, env_var):
         monkeypatch.setenv(env_var, str(pyenv_root_custom))
 
         assert get_pyenv_root() == pyenv_root_custom
 
 
-class TestGetPyenvVersionsDirectory(BaseTestClass):
+class TestGetPyenvVersionsDirectory:
 
     @pytest.fixture
-    def pyenv_root(tmp_path):
+    def pyenv_root(self, tmp_path):
         path = tmp_path / 'pyenv_root'
         path.mkdir()
         return path
 
     @pytest.fixture
-    def versions_dir(pyenv_root):
+    def versions_dir(self, pyenv_root):
         _versions_dir = pyenv_root / 'versions'
         _versions_dir.mkdir()
         return _versions_dir
 
     @pytest.fixture(autouse=True)
-    def setup(monkeypatch, pyenv_root):
+    def setup(self, monkeypatch, pyenv_root):
         monkeypatch.setattr(
             'pyenv_inspect.path.get_pyenv_root', lambda: pyenv_root)
 
-    def test_ok(versions_dir):
+    def test_ok(self, versions_dir):
         assert get_pyenv_versions_directory() == versions_dir
 
-    def test_error_does_not_exist(versions_dir):
+    def test_error_does_not_exist(self, versions_dir):
         versions_dir.rmdir()
         message = f'pyenv versions path does not exist: {versions_dir}'
 
         with pytest.raises(PathError, match=re.escape(message)):
             get_pyenv_versions_directory()
 
-    def test_error_not_a_directory(versions_dir):
+    def test_error_not_a_directory(self, versions_dir):
         versions_dir.rmdir()
         versions_dir.touch()
         message = f'pyenv versions path is not a directory: {versions_dir}'
@@ -131,16 +131,16 @@ class TestGetPyenvVersionsDirectory(BaseTestClass):
             get_pyenv_versions_directory()
 
 
-class BaseTestGetPyenvPythonExecutablePath(BaseTestClass):
+class BaseTestGetPyenvPythonExecutablePath:
 
     @pytest.fixture
-    def pyenv_root(tmp_path):
+    def pyenv_root(self, tmp_path):
         path = tmp_path / 'pyenv_root'
         path.mkdir()
         return path
 
     @pytest.fixture
-    def version_dir(pyenv_root):
+    def version_dir(self, pyenv_root):
         return pyenv_root / 'versions' / '3.10.4'
 
     @pytest.fixture
@@ -148,21 +148,21 @@ class BaseTestGetPyenvPythonExecutablePath(BaseTestClass):
         raise NotImplementedError
 
     @pytest.fixture(autouse=True)
-    def setup(exec_path):
+    def setup(self, exec_path):
         exec_path.parent.mkdir(parents=True)
 
-    def test_ok(version_dir, exec_path):
+    def test_ok(self, version_dir, exec_path):
         exec_path.touch(mode=0o777)
 
         assert get_pyenv_python_executable_path(version_dir) == exec_path
 
-    def test_error_does_not_exist(version_dir):
+    def test_error_does_not_exist(self, version_dir):
         message = f'pyenv python binary does not exist: {version_dir}'
 
         with pytest.raises(PathError, match=re.escape(message)):
             get_pyenv_python_executable_path(version_dir)
 
-    def test_error_not_a_file(version_dir, exec_path):
+    def test_error_not_a_file(self, version_dir, exec_path):
         exec_path.mkdir()
         message = f'pyenv python binary is not a file: {version_dir}'
 
@@ -176,10 +176,10 @@ class TestGetPyenvPythonExecutablePathPOSIX(
 ):
 
     @pytest.fixture
-    def exec_path(version_dir):
+    def exec_path(self, version_dir):
         return version_dir / 'bin' / 'python'
 
-    def test_error_not_executable(version_dir, exec_path):
+    def test_error_not_executable(self, version_dir, exec_path):
         exec_path.touch(mode=0o666)
         message = f'pyenv python binary is not executable: {version_dir}'
 
@@ -193,5 +193,5 @@ class TestGetPyenvPythonExecutablePathWindows(
 ):
 
     @pytest.fixture
-    def exec_path(version_dir):
+    def exec_path(self, version_dir):
         return version_dir / 'python.exe'
